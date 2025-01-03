8403ce46d680c88867268b7ae8811e092a21f813
Cabro <ozen.samet.57@gmail.com>
2025-01-04 01:04:25 +0300

    Version 2

--- src/main/java/tr/technicalserviceapp/App.java
+++ src/main/java/tr/technicalserviceapp/App.java
@@ -51,6 +51,7 @@ public class App {
             public void run() {
                 JFrame frame = new MainUI();
                 frame.setVisible(true);
+                System.out.println("Version 2");
             }
         });
     }
